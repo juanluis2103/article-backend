0a53cdbe8f3f91bb991684d650377d8f21eb56d5
Juan Luis Sierra <juanluissierra0321@gmail.com>
2025-10-31 00:58:14 -0500

    modified connection

--- connection/connection.js
+++ connection/connection.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 const connection = async() => {
     try {
-		await mongoose.connect("mongodb://localhost:27017/pruebadb")
+    const password = encodeURIComponent("CachotoroLeon123@.");
+    const uri = `mongodb+srv://juanchito:${password}@mycluster.9i6f5wb.mongodb.net/articles?retryWrites=true&w=majority&appName=mycluster`;
+		await mongoose.connect(uri)
 		console.log("Correctly connected to pruebadb database")
     } catch(err) {
         console.log(err);
